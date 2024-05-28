4bd4864b41ad083851330024a3b21abacaa8727c
愚指导 <yutingzhao1991@gmail.com>
2024-05-28 10:29:46 +0800

    feat: add new positions page (#72)

--- demo/pages/wtfswap/pool.tsx
+++ demo/pages/wtfswap/pool.tsx
@@ -1,5 +1,10 @@
 import WtfLayout from "@/components/WtfLayout";
+import Link from "next/link";
 
 export default function WtfswapPool() {
-  return <WtfLayout>WtfswapPool</WtfLayout>;
+  return (
+    <WtfLayout>
+      WtfswapPool <Link href="/wtfswap/positions">My Positions</Link>
+    </WtfLayout>
+  );
 }
