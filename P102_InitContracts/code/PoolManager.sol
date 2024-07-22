8741eadeb4a6d8d382f835a1f966f3669682aaf9
gaoyizhuo.gyz <gaoyizhuo.gyz@antgroup.com>
2024-07-22 10:11:41 +0800

    chore: update demo code

--- P102_InitContracts/code/PoolManager.sol
+++ P102_InitContracts/code/PoolManager.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-// Author: Eli Lee
 pragma solidity ^0.8.24;
 pragma abicoder v2;
 
@@ -8,12 +7,7 @@ import "./interfaces/IFactory.sol";
 import "./interfaces/IPool.sol";
 
 contract PoolManager is IPoolManager {
-
-    IFactory public factory;
-
-    constructor(address _factory) {
-        factory = IFactory(_factory);
-    }
+    address public factory;
 
     // 所有 PoolKey 的数组
     PoolKey[] private poolKeys;
@@ -27,20 +21,24 @@ contract PoolManager is IPoolManager {
     // 映射以检查某个 token 是否已添加到 tokens 数组中
     mapping(address => bool) private tokenExists;
 
+    constructor(address _factory) {
+        factory = _factory;
+    }
+
     function getPools()
-    external
-    view
-    override
-    returns (PoolKey[] memory pools)
+        external
+        view
+        override
+        returns (PoolKey[] memory pools)
     {
         return poolKeys;
     }
 
     function getTokens()
-    external
-    view
-    override
-    returns (address[] memory tokens)
+        external
+        view
+        override
+        returns (address[] memory tokens)
     {
         return tokenList;
     }
@@ -48,13 +46,11 @@ contract PoolManager is IPoolManager {
     function getTokenPools(
         address token
     ) external view override returns (PoolKey[] memory pools) {
-        //通过代币 index 和 poolKey index 找出代币关联的所有池子
         uint32[] memory indexes = tokenPoolIndexes[token];
-        PoolKey[] memory result = new PoolKey[](indexes.length);
+        pools = new PoolKey[](indexes.length);
         for (uint256 i = 0; i < indexes.length; i++) {
-            result[i] = poolKeys[indexes[i]];
+            pools[i] = poolKeys[indexes[i]];
         }
-        return result;
     }
 
     function getPoolInfo(
@@ -62,9 +58,8 @@ contract PoolManager is IPoolManager {
         address token1,
         uint24 fee
     ) external view override returns (PoolInfo memory poolInfo) {
-        //通过 IPool 构建 PoolInfo 的信息
-        address poolAddress = factory.getPool(token0, token1, fee);
-        require(poolAddress != address(0), "Pool does not exist");
+        address poolAddress = IFactory(factory).getPool(token0, token1, fee);
+        require(poolAddress != address(0), "PoolManager: POOL_NOT_FOUND");
         IPool pool = IPool(poolAddress);
         poolInfo = PoolInfo({
             feeProtocol: 0,
@@ -78,30 +73,23 @@ contract PoolManager is IPoolManager {
     function createAndInitializePoolIfNecessary(
         CreateAndInitializeParams calldata params
     ) external payable override returns (address pool) {
-        // 检查池子是否已存在
-        address existingPool = factory.getPool(params.token0, params.token1, params.fee);
-        if (existingPool != address(0)) {
-            return existingPool;
+        pool = IFactory(factory).getPool(params.token0, params.token1, params.fee);
+
+        if (pool != address(0)) {
+            return pool;
         }
 
-        // 创建新池子
-        pool = factory.createPool(params.token0, params.token1, params.fee);
+        pool = IFactory(factory).createPool(params.token0, params.token1, params.fee);
 
-        // 初始化池子
         IPool(pool).initialize(params.sqrtPriceX96, params.tickLower, params.tickUpper);
 
         // 添加 PoolKey 到 poolKeys 数组
-        PoolKey memory key = PoolKey({
+        PoolKey memory poolKey = PoolKey({
             token0: params.token0,
             token1: params.token1,
             fee: params.fee
         });
-        poolKeys.push(key);
-
-        //从代币地址映射到 PoolKeys
-        uint32 index = uint32(poolKeys.length - 1);
-        tokenPoolIndexes[params.token0].push(index);
-        tokenPoolIndexes[params.token1].push(index);
+        poolKeys.push(poolKey);
 
         // 检查并添加代币到 tokens 数组
         if (!tokenExists[params.token0]) {
@@ -112,6 +100,11 @@ contract PoolManager is IPoolManager {
             tokenList.push(params.token1);
             tokenExists[params.token1] = true;
         }
+
+        //从代币地址映射到 PoolKeys
+        uint32 index = uint32(poolKeys.length - 1);
+        tokenPoolIndexes[params.token0].push(index);
+        tokenPoolIndexes[params.token1].push(index);
 
         return pool;
     }
