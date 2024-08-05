86de20d7327bd7828af453a38b0afdf05207e404
tingzhao.ytz <yutingzhao1991@gmail.com>
2024-08-05 11:40:20 +0800

    feat: update init contract

--- P102_InitContracts/code/Pool.sol
+++ P102_InitContracts/code/Pool.sol
@@ -2,45 +2,84 @@
 pragma solidity ^0.8.24;
 
 import "./interfaces/IPool.sol";
+import "./interfaces/IFactory.sol";
 
 contract Pool is IPool {
-    function factory() external view override returns (address) {}
+    /// @inheritdoc IPool
+    address public immutable override factory;
+    /// @inheritdoc IPool
+    address public immutable override token0;
+    /// @inheritdoc IPool
+    address public immutable override token1;
+    /// @inheritdoc IPool
+    uint24 public immutable override fee;
+    /// @inheritdoc IPool
+    int24 public immutable override tickLower;
+    /// @inheritdoc IPool
+    int24 public immutable override tickUpper;
 
-    function token0() external view override returns (address) {}
+    /// @inheritdoc IPool
+    uint160 public override sqrtPriceX96;
+    /// @inheritdoc IPool
+    int24 public override tick;
+    /// @inheritdoc IPool
+    uint128 public override liquidity;
 
-    function token1() external view override returns (address) {}
+    // 用一个 mapping 来存放所有 Position 的信息
+    mapping(address => Position) public positions;
 
-    function fee() external view override returns (uint24) {}
+    constructor() {
+        // constructor 中初始化 immutable 的常量
+        // Factory 创建 Pool 时会通 new Pool{salt: salt}() 的方式创建 Pool 合约，通过 salt 指定 Pool 的地址，这样其他地方也可以推算出 Pool 的地址
+        // 参数通过读取 Factory 合约的 parameters 获取
+        // 不通过构造函数传入，因为 CREATE2 会根据 initcode 计算出新地址（new_address = hash(0xFF, sender, salt, bytecode)），带上参数就不能计算出稳定的地址了
+        (factory, token0, token1, tickLower, tickUpper, fee) = IFactory(
+            msg.sender
+        ).parameters();
+    }
 
-    function tickLower() external view override returns (int24) {}
-
-    function tickUpper() external view override returns (int24) {}
-
-    function sqrtPriceX96() external view override returns (uint160) {}
-
-    function tick() external view override returns (int24) {}
-
-    function liquidity() external view override returns (uint128) {}
-
-    function initialize(
-        uint160 sqrtPriceX96_,
-        int24 tickLower_,
-        int24 tickUpper_
-    ) external override {}
+    function initialize(uint160 sqrtPriceX96_) external override {
+        // 初始化 Pool 的 sqrtPriceX96
+        sqrtPriceX96 = sqrtPriceX96_;
+    }
 
     function mint(
         address recipient,
         uint128 amount,
         bytes calldata data
-    ) external override returns (uint256 amount0, uint256 amount1) {}
+    ) external override returns (uint256 amount0, uint256 amount1) {
+        // 基于 amount 计算出当前需要多少 amount0 和 amount1
+        // TODO 当前先写个假的
+        (amount0, amount1) = (amount / 2, amount / 2);
+        // 把流动性记录到对应的 position 中
+        positions[recipient].liquidity += amount;
+        // 回调 mintCallback
+        IMintCallback(recipient).mintCallback(amount0, amount1, data);
+        // TODO 检查钱到位了没有，如果到位了对应修改相关信息
+    }
 
     function collect(
         address recipient
-    ) external override returns (uint128 amount0, uint128 amount1) {}
+    ) external override returns (uint128 amount0, uint128 amount1) {
+        // 获取当前用户的 position，TODO recipient 应该改为 msg.sender
+        Position storage position = positions[recipient];
+        // TODO 把钱退给用户 recipient
+        // 修改 position 中的信息
+        position.tokensOwed0 -= amount0;
+        position.tokensOwed1 -= amount1;
+    }
 
     function burn(
         uint128 amount
-    ) external override returns (uint256 amount0, uint256 amount1) {}
+    ) external override returns (uint256 amount0, uint256 amount1) {
+        // 修改 positions 中的信息
+        positions[msg.sender].liquidity -= amount;
+        // 获取燃烧后的 amount0 和 amount1
+        // TODO 当前先写个假的
+        (amount0, amount1) = (amount / 2, amount / 2);
+        positions[msg.sender].tokensOwed0 += amount0;
+        positions[msg.sender].tokensOwed1 += amount1;
+    }
 
     function swap(
         address recipient,
--- P102_InitContracts/code/PoolManager.sol
+++ P102_InitContracts/code/PoolManager.sol
@@ -3,33 +3,35 @@ pragma solidity ^0.8.24;
 pragma abicoder v2;
 
 import "./interfaces/IPoolManager.sol";
+import "./Factory.sol";
 
-contract PoolManager is IPoolManager {
-    function getPools()
+contract PoolManager is Factory, IPoolManager {
+    Pair[] public pairs;
+
+    function getPairs() external view override returns (Pair[] memory) {
+        // 返回有哪些交易对，DApp 和 getAllPools 会用到
+        return pairs;
+    }
+
+    function getAllPools()
         external
         view
         override
-        returns (PoolKey[] memory pools)
-    {}
-
-    function getTokens()
-        external
-        view
-        override
-        returns (address[] memory tokens)
-    {}
-
-    function getTokenPools(
-        address token
-    ) external view override returns (PoolKey[] memory pools) {}
-
-    function getPoolInfo(
-        address token0,
-        address token1,
-        uint24 fee
-    ) external view override returns (PoolInfo memory poolInfo) {}
+        returns (PoolInfo[] memory poolsInfo)
+    {
+        // 遍历 pairs，获取当前有的所有的交易对
+        // 然后调用 Factory 的 getPools 获取每个交易对的所有 pool
+        // 然后获取每个 pool 的信息
+        // 然后返回
+        // 先 mock
+        poolsInfo = new PoolInfo[](pairs.length);
+        return poolsInfo;
+    }
 
     function createAndInitializePoolIfNecessary(
         CreateAndInitializeParams calldata params
-    ) external payable override returns (address pool) {}
+    ) external payable override returns (address pool) {
+        // 如果没有对应的 Pool 就创建一个 Pool
+        // 创建成功后记录到 pairs 中
+    }
 }
