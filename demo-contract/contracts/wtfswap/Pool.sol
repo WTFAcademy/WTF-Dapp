82263d901f93983f2f11332a7fe54a2fb8883a8e
tingzhao.ytz <yutingzhao1991@gmail.com>
2024-08-27 14:03:23 +0800

    feat: support Pool mint

--- demo-contract/contracts/wtfswap/Pool.sol
+++ demo-contract/contracts/wtfswap/Pool.sol
@@ -1,10 +1,21 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.24;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import "./libraries/SafeCast.sol";
+import "./libraries/SqrtPriceMath.sol";
+import "./libraries/TickMath.sol";
+import "./libraries/LiquidityMath.sol";
+import "./libraries/LowGasSafeMath.sol";
+
 import "./interfaces/IPool.sol";
 import "./interfaces/IFactory.sol";
 
 contract Pool is IPool {
+    using SafeCast for int256;
+    using LowGasSafeMath for uint256;
+
     /// @inheritdoc IPool
     address public immutable override factory;
     /// @inheritdoc IPool
@@ -43,19 +54,93 @@ contract Pool is IPool {
         sqrtPriceX96 = sqrtPriceX96_;
     }
 
+    struct ModifyPositionParams {
+        // the address that owns the position
+        address owner;
+        // any change in liquidity
+        int128 liquidityDelta;
+    }
+
+    function _modifyPosition(
+        ModifyPositionParams memory params
+    ) private returns (int256 amount0, int256 amount1) {
+        // 通过新增的流动性计算 amount0 和 amount1
+        // 参考 UniswapV3 的代码
+        amount0 = SqrtPriceMath.getAmount0Delta(
+            sqrtPriceX96,
+            TickMath.getSqrtRatioAtTick(tickUpper),
+            params.liquidityDelta
+        );
+        amount1 = SqrtPriceMath.getAmount1Delta(
+            TickMath.getSqrtRatioAtTick(tickLower),
+            sqrtPriceX96,
+            params.liquidityDelta
+        );
+
+        // 修改 liquidity
+        uint128 liquidityBefore = liquidity;
+        liquidity = LiquidityMath.addDelta(
+            liquidityBefore,
+            params.liquidityDelta
+        );
+    }
+
+    /// @dev Get the pool's balance of token0
+    /// @dev This function is gas optimized to avoid a redundant extcodesize check in addition to the returndatasize
+    /// check
+    function balance0() private view returns (uint256) {
+        (bool success, bytes memory data) = token0.staticcall(
+            abi.encodeWithSelector(IERC20.balanceOf.selector, address(this))
+        );
+        require(success && data.length >= 32);
+        return abi.decode(data, (uint256));
+    }
+
+    /// @dev Get the pool's balance of token1
+    /// @dev This function is gas optimized to avoid a redundant extcodesize check in addition to the returndatasize
+    /// check
+    function balance1() private view returns (uint256) {
+        (bool success, bytes memory data) = token1.staticcall(
+            abi.encodeWithSelector(IERC20.balanceOf.selector, address(this))
+        );
+        require(success && data.length >= 32);
+        return abi.decode(data, (uint256));
+    }
+
     function mint(
         address recipient,
         uint128 amount,
         bytes calldata data
     ) external override returns (uint256 amount0, uint256 amount1) {
+        require(amount > 0, "Mint amount must be greater than 0");
         // 基于 amount 计算出当前需要多少 amount0 和 amount1
-        // TODO 当前先写个假的
-        (amount0, amount1) = (amount / 2, amount / 2);
+        (int256 amount0Int, int256 amount1Int) = _modifyPosition(
+            ModifyPositionParams({
+                owner: recipient,
+                liquidityDelta: int256(int128(amount)).toInt128()
+            })
+        );
+        amount0 = uint256(amount0Int);
+        amount1 = uint256(amount1Int);
         // 把流动性记录到对应的 position 中
         positions[recipient].liquidity += amount;
+        // 记录用户的 tokenOwed0 和 tokenOwed1
+        positions[recipient].tokensOwed0 += amount0;
+        positions[recipient].tokensOwed1 += amount1;
+
+        uint256 balance0Before;
+        uint256 balance1Before;
+        if (amount0 > 0) balance0Before = balance0();
+        if (amount1 > 0) balance1Before = balance1();
         // 回调 mintCallback
         IMintCallback(recipient).mintCallback(amount0, amount1, data);
-        // TODO 检查钱到位了没有，如果到位了对应修改相关信息
+
+        if (amount0 > 0)
+            require(balance0Before.add(amount0) <= balance0(), "M0");
+        if (amount1 > 0)
+            require(balance1Before.add(amount1) <= balance1(), "M1");
+
+        emit Mint(msg.sender, recipient, amount, amount0, amount1);
     }
 
     function collect(
