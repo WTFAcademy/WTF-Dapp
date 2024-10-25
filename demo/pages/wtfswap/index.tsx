955f6d08466e78e827834a0c9c119446ed803f0a
smallfu6 <smallfu666@gmail.com>
2024-10-25 15:42:15 +0800

    refactor: simplify styles

--- demo/pages/wtfswap/index.tsx
+++ demo/pages/wtfswap/index.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { TokenSelect, type Token } from '@ant-design/web3';
-import { ETH, USDT } from '@ant-design/web3-assets/tokens';
-import { Card, Input, Button, Row, Col, Space, Typography } from 'antd';
+import { ETH, USDT, USDC } from '@ant-design/web3-assets/tokens';
+import { Card, Input, Button, Space, Typography } from 'antd';
 import { SwapOutlined } from '@ant-design/icons';
 
 import WtfLayout from "@/components/WtfLayout";
-import styles from "./styles.module.css";
+import styles from "./swap.module.css";
 
 const { Text } = Typography;
 
@@ -15,14 +15,14 @@ export default function Wtfswap() {
   const [token2, setToken2] = useState<Token>(USDT);
   const [amount1, setAmount1] = useState(0);
   const [amount2, setAmount2] = useState(0);
-
+  const [options1, setOptions1] = useState<Token[]>([ETH, USDT, USDC]);;
+  const [options2, setOptions2] = useState<Token[]>([USDT, ETH, USDC]);;
 
   const handleAmount1Change = (e: any) => {
     setAmount1(parseFloat(e.target.value));
     // todo: setAmount2
-    // const a2 = amount1 * 20;
-    // setAmount1(a2);
   };
+
 
   const handleSwitch = () => {
     setToken1(token2);
@@ -35,94 +35,61 @@ export default function Wtfswap() {
     // max 
   };
 
-
   return (
     <WtfLayout>
-      <Card title="Swap" className={styles.swapcard}>
-        <Row gutter={[16, 16]}>
-          <Col span={24}>
-            <Card>
-              <Space direction="vertical" style={{ width: '100%' }}>
-                <Input
-                  size="large"
-                  placeholder="0"
-                  variant="borderless"
-                  value={amount1}
-                  type="number"
-                  onChange={(e) => handleAmount1Change(e)}
-                  addonAfter={
-                    <TokenSelect value={token1} onChange={setToken1} options={[ETH, USDT]} />
-                  }
-                />
-
-                <Row style={{ width: '100%' }} align="middle">
-                  <Col flex="1">
-                    <Text type="secondary">
-                      $ 0.0
-                    </Text>
-                  </Col>
-                  <Col>
-                    <Space style={{ justifyContent: 'flex-end' }}>
-                      <Text type="secondary">
-                        Balance: 0
-                      </Text>
-                      <Button size="small" onClick={handleMax} type="link">
-                        Max
-                      </Button>
-                    </Space>
-                  </Col>
-                </Row>
-              </Space>
-            </Card>
-          </Col>
-
-          <Col span={24} style={{ textAlign: 'center' }}>
-            <Button
-              shape="circle"
-              icon={<SwapOutlined />}
-              onClick={handleSwitch}
-              style={{ fontSize: '20px' }}
-            />
-          </Col>
-
-          <Col span={24}>
-            <Card>
-              <Space direction="vertical" style={{ width: '100%' }}>
-                <Input
-                  size="small"
-                  placeholder="0"
-                  value={amount2}
-                  readOnly
-                  variant="borderless"
-                  addonAfter={
-                    <TokenSelect value={token2} onChange={setToken2} options={[ETH, USDT]} />
-                  }
-                />
-                <Row style={{ width: '100%' }} align="middle">
-                  <Col flex="1">
-                    <Text type="secondary">
-                      $ 0.0
-                    </Text>
-                  </Col>
-                  <Col>
-                    <Space style={{ justifyContent: 'flex-end' }}>
-                      <Text type="secondary">
-                        Balance: 0
-                      </Text>
-                    </Space>
-                  </Col>
-                </Row>
-              </Space>
-
-            </Card>
-          </Col>
-
-          <Col span={24}>
-            <Button type="primary" size="large" block>
-              Swap
-            </Button>
-          </Col>
-        </Row>
+      <Card title="Swap" className={styles.swapCard}>
+        <Card>
+          <Input
+            variant="borderless"
+            value={amount1}
+            type="number"
+            onChange={(e) => handleAmount1Change(e)}
+            addonAfter={
+              <TokenSelect value={token1} onChange={setToken1} options={options1} />
+            }
+          />
+          <Space className={styles.swapSpace}>
+            <Text type="secondary">
+              $ 0.0
+            </Text>
+            <Space>
+              <Text type="secondary">
+                Balance: 0
+              </Text>
+              <Button size="small" onClick={handleMax} type="link">
+                Max
+              </Button>
+            </Space>
+          </Space>
+        </Card>
+        <Space className={styles.switchBtn}>
+          <Button
+            shape="circle"
+            icon={<SwapOutlined />}
+            onClick={handleSwitch}
+          />
+        </Space>
+        <Card>
+          <Input
+            value={amount2}
+            variant="borderless"
+            type="number"
+            addonAfter={
+              <TokenSelect value={token2} onChange={setToken2} options={options2} />
+            }
+          />
+          <Space className={styles.swapSpace}>
+            <Text type="secondary">
+              $ 0.0
+            </Text>
+            <Text type="secondary">
+              Balance: 0
+            </Text>
+          </Space>
+        </Card>
+        <Button type="primary" size="large" block>
+          Swap
+        </Button>
       </Card>
     </WtfLayout>
   );
