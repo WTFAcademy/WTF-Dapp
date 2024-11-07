84435569fc106642101804448639e99b2601f9b3
smallfu6 <smallfu666@gmail.com>
2024-11-07 15:37:50 +0800

    feat: support a new approach in 04_CallContract(#122)

--- 04_CallContract/web3.tsx
+++ 04_CallContract/web3.tsx
@@ -1,48 +1,29 @@
-import {
-  Address,
-  ConnectButton,
-  Connector,
-  NFTCard,
-  useAccount,
-} from "@ant-design/web3";
-import { MetaMask, WagmiWeb3ConfigProvider } from "@ant-design/web3-wagmi";
-import { Button, message } from "antd";
 import { parseEther } from "viem";
-import { createConfig, http, useReadContract, useWriteContract } from "wagmi";
-import { mainnet } from "wagmi/chains";
-import { injected } from "wagmi/connectors";
+import { Button, message } from "antd";
+import { http, useReadContract, useWriteContract } from "wagmi";
+import { Mainnet, WagmiWeb3ConfigProvider, MetaMask } from '@ant-design/web3-wagmi';
+import { Address, NFTCard, ConnectButton, Connector, useAccount } from "@ant-design/web3";
 
-const config = createConfig({
-  chains: [mainnet],
-  transports: {
-    [mainnet.id]: http(),
-  },
-  connectors: [
-    injected({
-      target: "metaMask",
-    }),
-  ],
-});
 
 const CallTest = () => {
   const { account } = useAccount();
   const result = useReadContract({
     abi: [
       {
-        type: "function",
-        name: "balanceOf",
-        stateMutability: "view",
-        inputs: [{ name: "account", type: "address" }],
-        outputs: [{ type: "uint256" }],
+        type: 'function',
+        name: 'balanceOf',
+        stateMutability: 'view',
+        inputs: [{ name: 'account', type: 'address' }],
+        outputs: [{ type: 'uint256' }],
       },
     ],
-    // Sepolia test contract 0x418325c3979b7f8a17678ec2463a74355bdbe72c
-    address: "0xEcd0D12E21805803f70de03B72B1C162dB0898d9",
-    functionName: "balanceOf",
+    address: '0xEcd0D12E21805803f70de03B72B1C162dB0898d9',
+    functionName: 'balanceOf',
     args: [account?.address as `0x${string}`],
   });
-  const { writeContract } = useWriteContract();
 
+
+  const { writeContract } = useWriteContract();
   return (
     <div>
       {result.data?.toString()}
@@ -89,7 +70,13 @@ const CallTest = () => {
 
 export default function Web3() {
   return (
-    <WagmiWeb3ConfigProvider config={config} wallets={[MetaMask()]}>
+    <WagmiWeb3ConfigProvider
+      chains={[Mainnet]}
+      transports={{
+        [Mainnet.id]: http(),
+      }}
+      wallets={[MetaMask()]}
+    >
       <Address format address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9" />
       <NFTCard
         address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9"
