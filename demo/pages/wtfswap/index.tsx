cd3af68645029504c73cedc651c4f0e84ed97e73
tingzhao.ytz <yutingzhao1991@gmail.com>
2025-01-02 16:49:42 +0800

    feat: support quote

--- demo/pages/wtfswap/index.tsx
+++ demo/pages/wtfswap/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { TokenSelect, type Token } from "@ant-design/web3";
-import { Card, Input, Button, Space, Typography } from "antd";
+import { Card, Input, Button, Space, Typography, message } from "antd";
 import { SwapOutlined } from "@ant-design/icons";
 import { uniq } from "lodash-es";
 
@@ -8,54 +8,104 @@ import WtfLayout from "@/components/WtfLayout";
 import Balance from "@/components/Balance";
 import styles from "./swap.module.css";
 
+import { usePublicClient } from "wagmi";
+import { swapRouterAbi } from "@/utils/contracts";
+
 import {
   useReadPoolManagerGetPairs,
-  useWriteSwapRouterExactInput,
-  useWriteSwapRouterQuoteExactOutput,
-  useSimulateSwapRouterQuoteExactInput,
-  useSimulateSwapRouterQuoteExactOutput,
+  useReadIPoolManagerGetAllPools,
 } from "@/utils/contracts";
 import useTokenAddress from "@/hooks/useTokenAddress";
-import { getContractAddress, getTokenInfo } from "@/utils/common";
-import { TickMath } from "@uniswap/v3-sdk";
+import {
+  getContractAddress,
+  getTokenInfo,
+  parseAmountToBigInt,
+  parseBigIntToAmount,
+  computeSqrtPriceLimitX96,
+} from "@/utils/common";
+import { TickMath, encodeSqrtRatioX96 } from "@uniswap/v3-sdk";
 
 const { Text } = Typography;
 
 function Swap() {
+  // 用户选择的两个代币
   const [tokenA, setTokenA] = useState<Token>();
   const [tokenB, setTokenB] = useState<Token>();
+  // 两个代币的地址
   const tokenAddressA = useTokenAddress(tokenA);
   const tokenAddressB = useTokenAddress(tokenB);
+  // 按照地址大小排序
+  const [token0, token1] =
+    tokenAddressA && tokenAddressB && tokenAddressA < tokenAddressB
+      ? [tokenAddressA, tokenAddressB]
+      : [tokenAddressB, tokenAddressA];
+  // 是否是 token0 来交换 token1
+  const zeroForOne = token0 === tokenAddressA;
+  // 两个代币的数量
   const [amountA, setAmountA] = useState(0);
   const [amountB, setAmountB] = useState(0);
 
-  const { data: quoteAmountB, refetch: fetchQuoteAmountB } =
-    useSimulateSwapRouterQuoteExactInput({
-      address: getContractAddress("SwapRouter"),
-      args: [
-        {
-          tokenIn: tokenAddressA as `0x${string}`,
-          tokenOut: tokenAddressB as `0x${string}`,
-          indexPath: [0],
-          amountIn: isNaN(amountA)
-            ? BigInt(0)
-            : BigInt(amountA) * BigInt(10 ** (tokenA?.decimal || 18)),
-          sqrtPriceLimitX96:
-            BigInt(TickMath.MIN_SQRT_RATIO.toString()) + BigInt(1),
-        },
-      ],
-      query: {
-        enabled: false,
-      },
-    });
+  // 获取所有的交易对
+  const { data: pairs = [] } = useReadPoolManagerGetPairs({
+    address: getContractAddress("PoolManager"),
+  });
 
-  console.log("quoteAmountB", quoteAmountB?.result);
+  // 获取所有的代币信息
+  const options: Token[] = uniq(
+    pairs.map((pair) => [pair.token0, pair.token1]).flat()
+  ).map(getTokenInfo);
+
+  // 获取所有的交易池
+  const { data: pools = [] } = useReadIPoolManagerGetAllPools({
+    address: getContractAddress("PoolManager"),
+  });
+
+  // 计算交易池的交易顺序
+  const swapPools = pools.filter((pool) => {
+    return pool.token0 === token0 && pool.token1 === token1;
+  });
+  const swapIndexPath: number[] = swapPools
+    .sort((a, b) => {
+      // 简单处理，按照价格排序，优先在价格低的池子中交易
+      return a.sqrtPriceX96 > b.sqrtPriceX96 ? 1 : -1;
+    })
+    .map((pool) => pool.index);
+
+  // 计算本次交易的价格限制
+  const sqrtPriceLimitX96 = computeSqrtPriceLimitX96(swapPools, zeroForOne);
+
+  const publicClient = usePublicClient();
+
+  const updateAmountBWithAmountA = async (value: number) => {
+    if (!publicClient || !tokenAddressA || !tokenAddressB) {
+      return;
+    }
+    try {
+      const newAmountB = await publicClient.simulateContract({
+        address: getContractAddress("SwapRouter"),
+        abi: swapRouterAbi,
+        functionName: "quoteExactInput",
+        args: [
+          {
+            tokenIn: tokenAddressA,
+            tokenOut: tokenAddressB,
+            indexPath: swapIndexPath,
+            amountIn: parseAmountToBigInt(value, tokenA),
+            sqrtPriceLimitX96,
+          },
+        ],
+      });
+      setAmountB(parseBigIntToAmount(newAmountB.result, tokenB));
+    } catch (e: any) {
+      message.error(e.message);
+    }
+  };
 
   const handleAmountAChange = (e: any) => {
     const value = parseFloat(e.target.value);
     setAmountA(value);
     if (!isNaN(value)) {
-      fetchQuoteAmountB();
+      updateAmountBWithAmountA(value);
     }
   };
 
@@ -70,14 +120,6 @@ function Swap() {
     setAmountA(amountB);
     setAmountB(amountA);
   };
-
-  const { data: pairs = [] } = useReadPoolManagerGetPairs({
-    address: getContractAddress("PoolManager"),
-  });
-
-  const options: Token[] = uniq(
-    pairs.map((pair) => [pair.token0, pair.token1]).flat()
-  ).map(getTokenInfo);
 
   return (
     <Card title="Swap" className={styles.swapCard}>
--- demo/utils/common.ts
+++ demo/utils/common.ts
@@ -1,6 +1,8 @@
 import { encodeSqrtRatioX96 } from "@uniswap/v3-sdk";
 import type { Token } from "@ant-design/web3";
 import { Hardhat, Sepolia } from "@ant-design/web3-wagmi";
+import { TickMath } from "@uniswap/v3-sdk";
+import { maxBy, minBy } from "lodash-es";
 
 export const parsePriceToSqrtPriceX96 = (price: number): BigInt => {
   return BigInt(encodeSqrtRatioX96(price * 1000000, 1000000).toString());
@@ -108,4 +110,51 @@ export const getTokenInfo = (address: string): Token => {
       },
     ],
   };
+};
+
+// 把数字转化为大整数，支持 4 位小数
+export const parseAmountToBigInt = (amount: number, token?: Token): bigint => {
+  return BigInt(amount * 10000) * BigInt(10 ** ((token?.decimal || 18) - 4));
+};
+
+// 把大整数转化为数字，支持 4 位小数
+export const parseBigIntToAmount = (amount: bigint, token?: Token): number => {
+  return (
+    Number((amount / BigInt(10 ** ((token?.decimal || 18) - 4))).toString()) /
+    10000
+  );
+};
+
+export const computeSqrtPriceLimitX96 = (
+  pools: {
+    pool: `0x${string}`;
+    token0: `0x${string}`;
+    token1: `0x${string}`;
+    index: number;
+    fee: number;
+    feeProtocol: number;
+    tickLower: number;
+    tickUpper: number;
+    tick: number;
+    sqrtPriceX96: bigint;
+  }[],
+  zeroForOne: boolean
+): bigint => {
+  if (zeroForOne) {
+    // 如果是 token0 交换 token1，那么交易完成后价格 token0 变多，价格下降下限
+    // 先找到交易池的最小 tick
+    const minTick =
+      minBy(pools, (pool) => pool.tick)?.tick ?? TickMath.MIN_TICK;
+    // 价格限制为最小 tick - 100
+    const limitTick = Math.max(minTick - 100, TickMath.MIN_TICK);
+    return BigInt(TickMath.getSqrtRatioAtTick(limitTick).toString());
+  } else {
+    // 反之，设置一个最大的价格
+    // 先找到交易池的最大 tick
+    const maxTick =
+      maxBy(pools, (pool) => pool.tick)?.tick ?? TickMath.MAX_TICK;
+    // 价格限制为最大 tick + 100
+    const limitTick = Math.min(maxTick + 100, TickMath.MAX_TICK);
+    return BigInt(TickMath.getSqrtRatioAtTick(limitTick).toString());
+  }
 };
